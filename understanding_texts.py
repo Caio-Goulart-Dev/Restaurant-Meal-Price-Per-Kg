101129c4acf5deb63cdda93e32f4b90402b6d074
Caio-Goulart-Dev <caiox.gm@gmail.com>
2026-03-14 11:53:32 -0300

    Add Project Files

---
+++ understanding_texts.py
@@ -0,0 +1,16 @@
+text = 'this text has a linebreak \nhere. else here we have a \ttab'
+print (text)
+
+text = "Lowercase are still letters"
+print(text.capitalize())
+print(text.upper())
+print(text.lower())
+print(text.startswith('Low'))
+print(text.endswith('s'))
+print(text.count('L'))
+print('are' in text)
+
+print(text.replace("still", "of course"))
+
+
+
